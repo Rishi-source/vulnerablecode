968e7f6d19e33004a31fa59e765d099c0a367853
Rishi Garg <rishigarg2503@gmail.com>
2024-10-30 11:45:57 +0530

    Some Chnage
    Signed-off-by: Rishi Garg <rishigarg2503@gmail.com>

--- vulnerablecode/static/js/pagination.js
+++ vulnerablecode/static/js/pagination.js
@@ -10,6 +10,7 @@ function handlePageSizeChange(value) {
     if (search) {
         params.set('search', search);
     }
+    
     const newUrl = `${window.location.pathname}?${params.toString()}`;
     if (window.location.href !== newUrl) {
         window.location.href = newUrl;
